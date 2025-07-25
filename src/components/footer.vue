c4e82eecbf9e1626b94690ba0136b315e49c2622
Black-pirate-people <jibrilnation@gmail.com>
2025-07-25 11:43:13 +0700

    layout

---
+++ src/components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer
+    class="mt-10 p-4 text-sm text-center text-gray-500 border-t border-gray-200 dark:text-gray-400 dark:border-gray-700 sm:ml-64"
+  >
+    <span>© 2025 <a href="#" class="hover:underline">QL™</a>. All Rights Reserved.</span>
+  </footer>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import login from '../views/Auth/login.vue'
+import Layout from '../views/layouts/default-layout.vue'
+import Siswa from '../views/Siswa.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: login,
+    },
+    {
+      path: '/layout',
+      name: 'Layout',
+      component: Layout,
+      children : [
+        {
+          path: '/siswa',
+          name: 'Siswa',
+          component: Siswa,
+        }
+      ]
+    },
+  ],
+})  
+
+export default router
